3867395e77d5e439d1b5b4171f1e0b0960e98260
spersico <persi93@gmail.com>
2017-10-23 01:14:17 -0300

    Integrado boton y comportamiento UI de cargaArchivos - No funcionan las peticiones con archivos todavia

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ var vm = new Vue({
         resBody: '',
         responseHeaders: [],
         error: false,
+        archivo: false,
     },
     methods: {
         crearHeaderReq() { this.requestHeaders.push({ nombre: '', valor: '', activado: true }); },
@@ -15,6 +16,16 @@ var vm = new Vue({
             if (this.requestHeaders.length === 0) {
                 this.crearHeaderReq();
             }
+        },
+        cargarArchivo(evento) {
+            this.archivo = true;
+            this.request.data = evento.target.files[0];
+            console.log(this.request.data);
+        },
+        borrarArchivo() {
+            $("#inputArchivo").val('');
+            this.request.data = '';
+            this.archivo = false;
         },
         limpiarValorHeaderReq(i) { this.requestHeaders[i].valor = '' },
         validarReqHeader(reqHeader) { return reqHeader.activado && reqHeader.nombre !== '' && reqHeader.valor !== ''; },
@@ -25,33 +36,9 @@ var vm = new Vue({
                 if (this.request.domain[this.request.domain.length - 1] !== '/' && this.request.url[0] !== '/') {
                     this.request.domain += '/';
                 }
-                if (this.request.domain === 'test/') {
-                    this.request.domain = 'https://api.odonpad.com/api/';
-                    this.request.url = 'prestaciones';
-                    this.request.headers = [
-                        { nombre: 'Content-Type', valor: 'application/json', activado: true },
-                        { nombre: 'Clinica', valor: '59a59c88eac39554ef4de8ef', activado: true },
-                        { nombre: 'Authorization', valor: 'Ugbh6mHSgx7d3So6IlhEY6usvHJcPf4ECI9Y0zrrHVGbzxXs6e0CtOSWU3hQFaqR', activado: true }];
-                    this.requestHeaders = this.request.headers;
-                }
-                if (this.request.domain === 'testpost/') {
-                    this.request.domain = 'https://api.odonpad.com/api/';
-                    this.request.url = 'prestaciones';
-                    this.request.data = JSON.stringify(
-                        { "codigo": "pruebasHTTPSanti", 
-                        "nombre": "pruebasHTTPSanti", 
-                        "clinicaId": "59a59c88eac39554ef4de8ef", 
-                        "capituloId": "58d194cf24643f1a3851fa25", 
-                        "prestacionGenericaId": "58d1954c24643f1a3851fa27" });
-                    this.request.headers = [
-                        { nombre: 'Content-Type', valor: 'application/json', activado: true },
-                        { nombre: 'Clinica', valor: '59a59c88eac39554ef4de8ef', activado: true },
-                        { nombre: 'Authorization', valor: 'Ugbh6mHSgx7d3So6IlhEY6usvHJcPf4ECI9Y0zrrHVGbzxXs6e0CtOSWU3hQFaqR', activado: true }];
-                    this.requestHeaders = this.request.headers;
-                }
+                this.testHarcodeados();
                 this.realizarPeticion();
             }
-
         },
         realizarPeticion() {
             const headerOBJ = {};
@@ -74,6 +61,34 @@ var vm = new Vue({
             try { JSON.parse(txt); }
             catch (e) { return false; }
             return true;
+        },
+        testHarcodeados() {
+            if (this.request.domain === 'test/') {
+                this.request.domain = 'https://api.odonpad.com/api/';
+                this.request.url = 'prestaciones';
+                this.request.headers = [
+                    { nombre: 'Content-Type', valor: 'application/json', activado: true },
+                    { nombre: 'Clinica', valor: '59a59c88eac39554ef4de8ef', activado: true },
+                    { nombre: 'Authorization', valor: 'Ugbh6mHSgx7d3So6IlhEY6usvHJcPf4ECI9Y0zrrHVGbzxXs6e0CtOSWU3hQFaqR', activado: true }];
+                this.requestHeaders = this.request.headers;
+            }
+            if (this.request.domain === 'testpost/') {
+                this.request.domain = 'https://api.odonpad.com/api/';
+                this.request.url = 'prestaciones';
+                this.request.data = JSON.stringify(
+                    {
+                        "codigo": "pruebasHTTPSanti",
+                        "nombre": "pruebasHTTPSanti",
+                        "clinicaId": "59a59c88eac39554ef4de8ef",
+                        "capituloId": "58d194cf24643f1a3851fa25",
+                        "prestacionGenericaId": "58d1954c24643f1a3851fa27"
+                    });
+                this.request.headers = [
+                    { nombre: 'Content-Type', valor: 'application/json', activado: true },
+                    { nombre: 'Clinica', valor: '59a59c88eac39554ef4de8ef', activado: true },
+                    { nombre: 'Authorization', valor: 'Ugbh6mHSgx7d3So6IlhEY6usvHJcPf4ECI9Y0zrrHVGbzxXs6e0CtOSWU3hQFaqR', activado: true }];
+                this.requestHeaders = this.request.headers;
+            }
         }
     }
 })
