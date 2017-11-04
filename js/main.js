5ee728d0684a072b1556d81fdb9327383e610322
jeregonzalez <31245759+jeregonzalez@users.noreply.github.com>
2017-11-04 03:30:57 -0300

    Cambios en interfaz y agregado de funciones

--- js/main.js
+++ js/main.js
@@ -2,24 +2,34 @@ var vm = new Vue({
     el: '#petitioner',
     data: {
         request: { domain: '', url: '', method: 'GET', data: '', headers: [] },
+        requestParams: [],
         requestHeaders: [],
-        resBody: '',
         responseHeaders: [],
-        error: false,
+        responseBody: '',
+        statusResponse: {status:'', statusText:''},
+        newRequestParam: {nombre:'', valor:''},
+        newRequestHeader: {nombre:'', valor:''},
         archivo: false,
-        height: (screen.height*0.5),
-        newRequestHeader: {nombre:'', valor:''}
+        error: false,
+        heightPetition: (screen.height*0.5),
+        heightResponse: (screen.height*0.65)
     },
     methods: {
-        crearHeaderReq() {
-            if(this.newRequestHeader.nombre === '' || this.newRequestHeader.valor === '')
+        addKey(isParam){
+            var array = (isParam)? this.requestParams : this.requestHeaders;
+            var newKey = (isParam)? this.newRequestParam : this.newRequestHeader;
+            if(newKey.nombre === '' || newKey.valor === '')
                 return false;
-            this.requestHeaders.push({ nombre: this.newRequestHeader.nombre, valor: this.newRequestHeader.valor, activado: true, focus: false });
-            this.newRequestHeader.nombre = '';
-            this.newRequestHeader.valor = '';
+            array.push({ nombre: newKey.nombre, valor: newKey.valor, activado: true, focus: false });
+            newKey.nombre = '';
+            newKey.valor = '';
         },
-        borrarHeaderReq(i) {
-            this.requestHeaders.splice(i, 1);
+        removeKey(isParam, i){
+            var array = (isParam)? this.requestParams : this.requestHeaders;
+            array.splice(i, 1);
+        },
+        getEstado(){
+            return "Estado: " + this.statusResponse.status + " - " + this.statusResponse.statusText.toUpperCase();
         },
         cargarArchivo(evento) {
             this.archivo = true;
@@ -31,35 +41,52 @@ var vm = new Vue({
             this.request.data = '';
             this.archivo = false;
         },
-        limpiarValorHeaderReq(i) { this.requestHeaders[i].valor = '' },
         validarReqHeader(reqHeader) { return reqHeader.activado && reqHeader.nombre !== '' && reqHeader.valor !== ''; },
-        validarDatos() {
+        enviarPeticion() {
             this.request.headers = [];
-            this.requestHeaders.map((x) => { if (this.validarReqHeader(x)) { this.request.headers.push(x) } })
-            if (this.request.domain.length > 0 || this.request.url.length > 0) {
-                if (this.request.domain[this.request.domain.length - 1] !== '/' && this.request.url[0] !== '/') {
-                    this.request.domain += '/';
-                }
-                this.testHarcodeados();
-                this.realizarPeticion();
-            }
+            this.requestHeaders.map((x) => { if (this.validarReqHeader(x)) { this.request.headers.push(x); } });
+            this.sendHTTP();
         },
-        realizarPeticion() {
+        sendHTTP(){
             const headerOBJ = {};
-            this.request.headers.map((x) => { headerOBJ[x.nombre] = x.valor });
-
+            this.request.headers.map((x) => { headerOBJ[x.nombre] = x.valor; });
             $.ajax({
                 url: '' + this.request.domain + this.request.url,
                 method: this.request.method,
-                data: this.request.data,
+                data: (this.request.method === 'GET')? '' : this.request.data,
                 headers: headerOBJ,
-                success: (data) => {
-                    this.resBody = JSON.stringify(data).split(",").join(",\n");
+                success: function(data, status, xhr){
+                    this.error = false;
+                    vm.setResponse(data, status, xhr);
                 },
-                error: (data) => {
-                    this.resBody = JSON.stringify(data);
-                },
+                error: function(data, status, xhr){
+                    this.error = true;
+                    vm.setResponse(data, status, xhr);
+                }
             });
+        },
+        setResponse(data, status, xhr){
+            this.responseHeaders = this.formatHeaders(xhr.getAllResponseHeaders());
+            this.responseBody = xhr.responseText;
+            var formatedText = $("<pre class='brush: html'></pre>");
+            formatedText.html(this.responseBody.replace(/</g,"&lt;"));
+            $("#responseText").html(formatedText);
+            SyntaxHighlighter.highlight();
+            $("#responseFrame").attr("srcdoc", this.responseBody);
+            this.statusResponse.status = xhr.status;
+            this.statusResponse.statusText = xhr.statusText;
+        },
+        formatHeaders(text){
+            var headers = {};
+                text.split('\u000d\u000a').forEach((line) => {
+                    if (line.length > 0)
+                    {
+                        var delimiter = '\u003a\u0020',
+                        header = line.split(delimiter);
+                        headers[header.shift().toLowerCase()] = header.join(delimiter);
+                    }
+                });
+            return headers;
         },
         esJsonString(txt) {
             try { JSON.parse(txt); }
